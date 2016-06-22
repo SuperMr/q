bfce5071ea2c990706640d96a70f6ee36b7fddca
super <super@zhangguanchaoMacBookPro.local>
2016-06-22 14:08:11 +0800

    项目

---
+++ qq/qq/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qq
+//
+//  Created by super on 16/6/22.
+//  Copyright © 2016年 super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qq/qq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qq
+//
+//  Created by super on 16/6/22.
+//  Copyright © 2016年 super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
